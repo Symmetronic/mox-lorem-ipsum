aceae3418bb40242ed1c7f17013e11bc216cb120
Simon Hochholzer <simon@symmetronic.com>
2018-09-24 11:02:46 +0200

    update component

--- src/components/lorem-ipsum/lorem-ipsum.spec.ts
+++ src/components/lorem-ipsum/lorem-ipsum.spec.ts
@@ -7,13 +7,42 @@ describe('lorem-ipsum', () => {
   });
 
   describe('rendering', () => {
-    let element: HTMLElement;
+    let element: HTMLLoremIpsumElement;
     let testWindow: TestWindow;
     beforeEach(async () => {
       testWindow = new TestWindow();
       element = await testWindow.load({
         components: [LoremIpsum],
-        html: '<lorem-ipsum></lorem-ipsum>'
+        html: '<mox-lorem-ipsum></mox-lorem-ipsum>'
+      });
+    });
+
+    it('should work without parameters', () => {
+      expect(element.textContent).not.toBe('');
+      expect(element.querySelectorAll('p').length).toBe(1);
+    });
+
+    describe('count', () => {
+      it('should not display any paragraphs for counts less than 1', async () => {
+        element.count = 0;
+        await testWindow.flush();
+        expect(element.textContent).toBe('');
+        expect(element.querySelectorAll('p').length).toBe(0);
+      });
+
+      it('should display one paragraph for invalid counts', async () => {
+        element.count = 'three';
+        await testWindow.flush();
+        expect(element.textContent).not.toBe('');
+        expect(element.querySelectorAll('p').length).toBe(1);
+      });
+
+      it('should work with counts larger than 1', async () => {
+        let count: number = 3;
+        element.count = count;
+        await testWindow.flush();
+        expect(element.textContent).not.toBe('');
+        expect(element.querySelectorAll('p').length).toBe(count);
       });
     });
   });
--- src/components/lorem-ipsum/lorem-ipsum.tsx
+++ src/components/lorem-ipsum/lorem-ipsum.tsx
@@ -1,7 +1,7 @@
-import { Component, Prop, State } from '@stencil/core';
+import { Component, Prop, State, Watch } from '@stencil/core';
 
 /**
- * Web component to create paragraphs containing blind text.
+ * Web component to create paragraphs containing filler text.
  */
 @Component({
   tag: 'mox-lorem-ipsum',
@@ -11,6 +11,27 @@ import { Component, Prop, State } from '@stencil/core';
 export class LoremIpsum {
 
   /**
+   * The default count to set if no valid value is specified.
+   */
+  DEFAULT_COUNT: number = 1;
+
+  /**
+   * Collection of filler text paragraphs.
+   */
+  LOREM_IPSUM_PARAGRAPHS: string[] = [
+    'Enim laborum dolor dolor sint ea laboris veniam nulla aute incididunt dolor pariatur do.',
+    'Ex officia ex eu ad aliqua voluptate qui pariatur.',
+    'Tempor consequat ipsum mollit est voluptate est mollit in Lorem tempor labore voluptate officia commodo do.',
+    'Ipsum minim qui deserunt laboris sit consectetur laborum et ex do est quis magna commodo aliquip officia.',
+    'Excepteur ex deserunt consequat deserunt elit consectetur ea sunt ut ea enim culpa magna anim voluptate cupidatat.',
+    'In cupidatat quis adipisicing labore esse aliquip velit non deserunt nostrud.',
+    'Cillum ad enim amet consectetur amet non in voluptate tempor laborum enim eiusmod.',
+    'Quis eiusmod eu esse cupidatat et voluptate eiusmod sit commodo id labore qui.',
+    'Nulla magna pariatur pariatur do ut eiusmod amet culpa cupidatat laborum do dolor aliquip ipsum eiusmod.',
+    'Incididunt est est nulla sit exercitation non minim ex esse enim commodo laborum commodo ea reprehenderit occaecat.'
+  ];
+
+  /**
    * The displayed paragraphs.
    */
   @State() paragraphs: string[] = [];
@@ -18,16 +39,47 @@ export class LoremIpsum {
   /**
    * The number of paragraphs to create.
    */
-  @Prop() count: number = 1;
+  @Prop() count: number;
+
+  /**
+   * Watches for changes in the count property.
+   */
+  @Watch('count')
+  countChanged() {
+    this.updateState();
+  }
 
   /**
    * Action before the component loaded.
    */
   componentWillLoad(): void {
-    for (let i: number = 0; i < this.count; i++) {
-      let paragraph: string = 'Cillum ut ad incididunt eiusmod amet esse deserunt incididunt eiusmod incididunt.';
-      this.paragraphs = [...this.paragraphs, paragraph];
+    this.updateState();
+  }
+
+  /**
+   * Returns a random string of filler text.
+   * @return A random string of filler text.
+   */
+  randomParagraph(): string {
+    return this.LOREM_IPSUM_PARAGRAPHS[Math.floor(
+        Math.random() * this.LOREM_IPSUM_PARAGRAPHS.length)];
+  }
+
+  /**
+   * Updates the paragraphs displayed in the component.
+   */
+  updateState(): void {
+    /* Initialize values. */
+    let count: number = (isNaN(this.count)) ? this.DEFAULT_COUNT : this.count;
+    let paragraphs: string[] = [];
+
+    /* Determine paragraphs. */
+    for (let i: number = 0; i < count; i++) {
+      paragraphs.push(this.randomParagraph());
     }
+
+    /* Update paragraphs. */
+    this.paragraphs = paragraphs;
   }
 
   /**
